3cf816a34c504a02534684a5f9d0c5aca797a5c1
Airborne4 <league@wts22.attlocal.net>
2019-03-09 14:31:09 -0800

    Own Adventure

--- src/extra/OwnAdventure.java
+++ src/extra/OwnAdventure.java
@@ -1,10 +1,12 @@
 package extra;
 
+import javax.swing.JOptionPane;
+
 public class OwnAdventure {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		JOptionPane.showOptionDialog(null, "Pop quiz! What is pi + pi?", "Pop Quiz", "No answer", "6.28318530718", "i");
 	}
 
 }
