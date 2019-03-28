2bc19c2a566b4b0e075c183f8ad94906a77e73a2
TheNeighbor <league@wts13.attlocal.net>
2019-03-27 17:05:17 -0700

    Own Adventure: IP

--- src/extra/OwnAdventure.java
+++ src/extra/OwnAdventure.java
@@ -7,17 +7,23 @@ public class OwnAdventure {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String popquiz = JOptionPane.showInputDialog("Pop quiz! You got a loan for $5000 at 5% interest rate for 6 years. What is the amount to be repaid? (No calculators.)");
-		if (popquiz.equals("6500")||popquiz.equals("$6500")||popquiz.equals("$6,500")||popquiz.equals("6,500")) {
-			String pass = JOptionPane.showInputDialog("Congragulations! You passed the quiz. Now it is time to take the SAT. What is 1+1=?");
-			if (pass.equalsIgnoreCase("window")) {
-				
+		String popquiz = JOptionPane.showInputDialog(
+				"Pop quiz! You got a loan for $5,000 at a 5% interest rate for 6 years. What is the amount to be repaid? (No calculators.)");
+		if (popquiz.equals("6500") || popquiz.equals("$6500") || popquiz.equals("$6,500") || popquiz.equals("6,500")) {
+			String passQ1 = JOptionPane.showInputDialog(
+					"Congragulations! You passed the quiz. Now it is time to take the SAT. What is 1+1=?");
+			if (passQ1.equalsIgnoreCase("window")) {
+				JOptionPane.showMessageDialog(null, "Good, last question.");
 			}
+		} else {
+			JOptionPane.showMessageDialog(null, "You failed! You'll never go to college. You are considered an entity.");
 		}
-		else {
-			JOptionPane.showMessageDialog(null, "You failed! You'll never go to college. Your socioeconomic status is now -999999999999999999999999999999999999999.");
+		String q2 = JOptionPane.showInputDialog("What is 2+2?");
+		if (q2.equalsIgnoreCase("fish")) {
+			JOptionPane.showMessageDialog(null, "Congragulations! You passed the SAT. You're going to college!");
 		}
-
+		
+		
 	}
-	
+
 }
