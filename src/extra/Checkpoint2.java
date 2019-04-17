550875d32117ddb96d47e22d2e26e61475e519e5
TheNeighbor <league@wts13.attlocal.net>
2019-04-17 16:58:54 -0700

    Checkpoints 2 and 3 :smile:

--- src/extra/Checkpoint2.java
+++ src/extra/Checkpoint2.java
@@ -1,10 +1,26 @@
 package extra;
 
+import javax.swing.JOptionPane;
+
 public class Checkpoint2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+String flour = JOptionPane.showInputDialog("How cups of flour do you have?");
+int flournum = Integer.parseInt(flour);
+if(flournum<2) {
+	JOptionPane.showMessageDialog(null, "Go to the store and buy some more.");
+}
+String friends = JOptionPane.showInputDialog("How many people are you giving cookies to?");
+int friendsnum = Integer.parseInt(friends);
+if (friendsnum>30) {
+	JOptionPane.showMessageDialog(null, "You will have to bake two batches of cookies!");
+}
+String batches = JOptionPane.showInputDialog("How many batches of cookies have you baked?");
+int batchesnum = Integer.parseInt(batches);
+if(batchesnum==2) {
+	JOptionPane.showMessageDialog(null, "You are lucky to have so many friends.");
+}
 	}
 
 }
---
+++ src/extra/Checkpoint3.java
@@ -0,0 +1,13 @@
+package extra;
+
+public class Checkpoint3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i = 0; i < 501; i++) {
+	if(i%2==1)
+		System.out.println(i);
+}
+	}
+
+}
